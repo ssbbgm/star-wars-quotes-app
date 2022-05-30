a36b8da90dc8cd25bd6628847904c92056b3b26e
Sherreka <rekadarnb@gmail.com>
2022-05-30 14:47:51 -0400

    used promises on db

--- server.js
+++ server.js
@@ -1,20 +1,25 @@
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
+const cli = require('nodemon/lib/cli');
 const MongoClient = require('mongodb').MongoClient;
 const app = express();
 const PORT = process.env.PORT || 3001;
 const DB_CONN = process.env.DB_CONN;
 
 
-
 app.use(bodyParser.urlencoded({ extended: true }));
 
-MongoClient.connect(DB_CONN, (err, client) => {
-    if(err) return console.log(err);
-    console.log(`Database connected`)
-});
-
+MongoClient.connect(DB_CONN)
+    .then(client => {
+        console.log('Connected to Database')
+        const db = client.db('star-wars-quotes');
+        app.use()
+        app.get()
+        app.post()
+        app.listen()
+    })
+    .catch(error => console.error(error));
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
