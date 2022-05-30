6d6e306975232e04d5b9169730f000c1a9254a80
Sherreka <rekadarnb@gmail.com>
2022-05-30 14:32:03 -0400

    added database

--- server.js
+++ server.js
@@ -1,11 +1,28 @@
+require('dotenv').config();
 const express = require('express');
+const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
 const app = express();
 const PORT = process.env.PORT || 3001;
+const DB_CONN = process.env.DB_CONN;
+
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+MongoClient.connect(DB_CONN, (err, client) => {
+    if(err) return console.log(err);
+    console.log(`Database connected`)
+});
 
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
 });
+
+app.post('/quotes', (req, res) => {
+    console.log(req.body);
+})
 
 app.listen(PORT, function (){
     console.log(`Now listening on ${PORT}`)
