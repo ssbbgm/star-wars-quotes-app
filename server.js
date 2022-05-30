cdb714794f8de2a2deed02780c5c8d9b23ba9f62
Sherreka <rekadarnb@gmail.com>
2022-05-30 17:56:37 -0400

    added the ejs for views

--- server.js
+++ server.js
@@ -1,34 +1,43 @@
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
-const cli = require('nodemon/lib/cli');
+const { render } = require('express/lib/response');
 const MongoClient = require('mongodb').MongoClient;
 const app = express();
 const PORT = process.env.PORT || 3001;
 const DB_CONN = process.env.DB_CONN;
 
 
-app.use(bodyParser.urlencoded({ extended: true }));
-
 MongoClient.connect(DB_CONN)
     .then(client => {
         console.log('Connected to Database')
         const db = client.db('star-wars-quotes');
-        app.use()
-        app.get()
-        app.post()
-        app.listen()
+        const quotesCollection = db.collection('quotes');
+
+        app.set('view engine', 'ejs');
+
+        app.use(bodyParser.urlencoded({ extended: true }));
+        app.use(express.json());
+
+        app.get('/', (req, res) => {
+            db.collection('quotes').find().toArray()
+            .then(results => {
+                res.render('index.ejs', { quotes: results })
+            })
+            .catch(error => console.log(error))
+        });
+        
+        app.post('/quotes', (req, res) => {
+                quotesCollection.insertOne(req.body)
+                .then(result => {
+                    res.redirect('/')
+                })
+                .catch(error => console.error(error))
+        })
+        
+        app.listen(PORT, function (){
+            console.log(`Now listening on ${PORT}`)
+        });
     })
     .catch(error => console.error(error));
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html')
-});
-
-app.post('/quotes', (req, res) => {
-    console.log(req.body);
-})
-
-app.listen(PORT, function (){
-    console.log(`Now listening on ${PORT}`)
-});
