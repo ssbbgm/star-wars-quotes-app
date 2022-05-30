5e850327de271e0ad9fde8597f796cdff21727dd
Sherreka <rekadarnb@gmail.com>
2022-05-30 13:20:41 -0400

    added html

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+});
+
 app.listen(PORT, function (){
     console.log(`Now listening on ${PORT}`)
-})
+});
